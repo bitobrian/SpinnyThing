693d7dc7d4e72c7d33765a53fd9001e31559765a
test2 <nairbsd@gmail.com>
2015-02-02 13:04:26 -0600

    Commit

---
+++ Yus/Yus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Yus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool wut = true;
+
+            while(wut)
+            {
+                Console.WriteLine("What is your favorite color?");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("NO");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
+        }
+    }
+}
