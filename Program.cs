f7cbfd8b84f92abcefe400fd0f09cbf6dd03e44f
Brian Donaldson <nairbsd@gmail.com>
2020-01-15 21:27:45 -0600

    Upgrade to .net core 3.1. Update program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace SpinnyThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spinSpeed = 75;
+            while (true)
+            {
+                Console.WriteLine(@"\");
+                Thread.Sleep(spinSpeed);
+                Console.Clear();
+
+                Console.WriteLine(@"|");
+                Thread.Sleep(spinSpeed);
+                Console.Clear();
+
+                Console.WriteLine(@"/");
+                Thread.Sleep(spinSpeed);
+                Console.Clear();
+
+                Console.WriteLine(@"-");
+                Thread.Sleep(spinSpeed);
+                Console.Clear();
+            }
+        }
+    }
+}
